fbb7b0cdb81b47d90a030886bb1875d7c8a914e0
mythosmod <91185016+mythosmod@users.noreply.github.com>
2023-06-03 15:20:46 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -441,23 +441,26 @@ Anyway?`,
     <p>
       <InterpolatedTranslation
         str={t(
-          `The Original Guide for CDDA is developed on {link_github} by {link_nornagon}, this version is a Bright Nights specific fork by {link_mythosmod}. If you notice any problems, please {link_file_an_issue}!`,
+          `The Original Guide for {link_cdda} is developed on {link_github} by {link_nornagon}, this version is a Bright Nights specific fork by {link_mythosmod}. If you notice any problems with tjis version, please {link_file_an_issue}!`,
           {
             link_github: "{link_github}",
             link_nornagon: "{link_nornagon}",
 			link_mythosmod: "{link_mythosmod}",
             link_file_an_issue: "{link_file_an_issue}",
+			link_cdda: "{link_cdda}",
           }
         )}
         slot0="link_github"
         slot1="link_nornagon"
 		slot2="link_mythosmod"
         slot3="link_file_an_issue">
+		slot4="link_cdda">
         <a slot="0" href="https://github.com/nornagon/cdda-guide">GitHub</a>
         <a slot="1" href="https://www.nornagon.net">nornagon</a>
 		<a slot="2" href="https://github.com/mythosmod/cbn-guide">MythosMod</a>
         <a slot="3" href="https://github.com/mythosmod/cbn-guide/issues"
           >{t("file an issue")}</a>
+        <a slot="4" href="https://cataclysmdda.org/">Cataclysm: Dark Days Ahead</a>
       </InterpolatedTranslation>
     </p>
 
