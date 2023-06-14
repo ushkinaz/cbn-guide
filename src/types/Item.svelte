34e51cc809fb0d175aefe4f42718b26dbc93733f
mythosmod <91185016+mythosmod@users.noreply.github.com>
2023-06-14 17:52:50 +0100

    Exlude spare parts from ammo volume calculation

--- src/types/Item.svelte
+++ src/types/Item.svelte
@@ -211,7 +211,7 @@ const grantedByMutation = data
   .filter((m) => m.id && m.integrated_armor?.some((x) => x === item.id));
 
 function normalizeStackVolume(item: Item): (string | number) | undefined {
-  if (item.type === "AMMO") {
+  if (item.type === "AMMO" && item.category !== "spare_parts") {
     const { count } = item;
     return `${parseVolume(item.volume ?? "1 ml") / (count ?? 1)} ml`;
   }
