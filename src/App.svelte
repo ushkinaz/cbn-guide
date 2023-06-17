6d71ab3e2d9823c61d3375f1ceebc595787f8ff1
Jeremy Rose <nornagon@nornagon.net>
2023-06-16 17:23:48 -0700

    switch role=search to type=search on search box
    (Backported from nornagon/cdda-guide@3acac48b1e584ab1089ef36c588f14ef8ba7e93f)

--- src/App.svelte
+++ src/App.svelte
@@ -310,7 +310,7 @@ function langHref(lang: string, href: string) {
         placeholder={t("Search...", {
           _comment: "Placeholder text in the search box",
         })}
-        role="search"
+        type="search"
         bind:value={search}
         on:input={clearItem}
         id="search" />
