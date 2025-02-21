bf4760e54b142b8d3d7e6b1eca18d57b66cc5692
ushkinaz <398781+ushkinaz@users.noreply.github.com>
2025-02-21 16:50:15 +0400

    fixes sentry integration, improves prod mode detection. Actual integration is not working yet, needs updated key

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import App from "./App.svelte";
 import * as Sentry from "@sentry/browser";
-import { Integrations } from "@sentry/tracing";
 import "@fontsource/unifont";
 import { registerSW } from "virtual:pwa-register";
 import { tx } from "@transifex/native";
+import { browserTracingIntegration } from "@sentry/browser";
 
 tx.init({
   token: "1/1d8c1f9e14b4c21d70dd3f6fccdd0ab16b691105",
 });
 
-if (location.hostname !== "localhost")
+if (import.meta.env.PROD)
   Sentry.init({
     dsn: "https://e7e132477a2844118b8f6d045a507e10@o318291.ingest.sentry.io/5665093",
-    integrations: [new Integrations.BrowserTracing()],
+    integrations: [browserTracingIntegration],
     tracesSampleRate: 0.2,
     ...(process.env.GITHUB_SHA && {
       release: `cdda-guide@${process.env.GITHUB_SHA.slice(0, 8)}`,
