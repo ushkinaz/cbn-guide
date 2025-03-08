ed9f9fc301282d800f2fc4eee05d65c4f80439c4
ushkinaz <398781+ushkinaz@users.noreply.github.com>
2025-03-08 21:43:42 +0400

    Switched to separated sentry.io, upgraded packages

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,27 @@
 import App from "./App.svelte";
 import * as Sentry from "@sentry/browser";
+import { browserTracingIntegration } from "@sentry/browser";
 import "@fontsource/unifont";
 import { registerSW } from "virtual:pwa-register";
 import { tx } from "@transifex/native";
-import { browserTracingIntegration } from "@sentry/browser";
 
 tx.init({
   token: "1/1d8c1f9e14b4c21d70dd3f6fccdd0ab16b691105",
 });
 
-if (import.meta.env.PROD)
+if (import.meta.env.PROD) {
+  //The SHA1 is taken from either the GitHub var or the Cloudflare var.
+  const commitSHA =
+    process.env.CF_PAGES_COMMIT_SHA ?? process.env.GITHUB_SHA ?? "none";
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     integrations: [browserTracingIntegration],
     tracesSampleRate: 0.2,
-    ...(process.env.GITHUB_SHA && {
-      release: `cdda-guide@${process.env.GITHUB_SHA.slice(0, 8)}`,
+    ...(commitSHA && {
+      release: `cdda-guide@${commitSHA.slice(0, 8)}`,
     }),
   });
+}
 
 registerSW({});
 
