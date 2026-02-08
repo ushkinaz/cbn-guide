307ed0bf5ec0599923cd651c039d1c922e581f2d
ushkinaz <398781+ushkinaz@users.noreply.github.com>
2026-02-09 02:22:30 +0400

    fix(styles): fix redundant `font-family` declaration in `CategoryGrid.svelte`

--- src/CategoryGrid.svelte
+++ src/CategoryGrid.svelte
@@ -257,12 +257,7 @@ img.category-icon {
   font-size: 0.9rem;
   font-weight: bold;
   font-family:
-    "Spline Sans Mono",
-    Menlo,
-    Monaco,
-    Consolas,
-    "Courier New",
-    monospace font-size;
+    "Spline Sans Mono", Menlo, Monaco, Consolas, "Courier New", monospace;
   margin: 0;
 
   text-shadow: 0 1px 2px rgba(0, 0, 0, 0.8);
