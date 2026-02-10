c6cc7156667c99c08a17cc1f064e7498907ab4da
ushkinaz <398781+ushkinaz@users.noreply.github.com>
2026-02-10 19:44:10 +0400

    fix: support optional cooldown for special attacks

--- src/types/monster/special_attacks/GunAttack.svelte
+++ src/types/monster/special_attacks/GunAttack.svelte
@@ -5,4 +5,7 @@ import ItemLink from "../../ItemLink.svelte";
 export let attack: GunAttack & { cooldown?: number };
 </script>
 
-<ItemLink type="item" id={attack.gun_type} showIcon={false} /> (cooldown: {attack.cooldown})
+<ItemLink
+  type="item"
+  id={attack.gun_type}
+  showIcon={false} />{#if attack.cooldown !== undefined}{" "}(cooldown: {attack.cooldown}){/if}
