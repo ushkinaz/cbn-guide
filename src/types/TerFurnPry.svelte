5b8e7c7fe9dd9700010dfafa997219c18bddac47
ushkinaz <398781+ushkinaz@users.noreply.github.com>
2026-04-17 00:30:09 +0400

    fix(types): update pry details display and reorganize break item logic in UI

--- src/types/TerFurnPry.svelte
+++ src/types/TerFurnPry.svelte
@@ -58,17 +58,31 @@ const _comment = "prying";
     <ThingLink id="PRY" type="tool_quality" showIcon={false} />
     {pry.pry_quality ?? 0}
   </dd>
-  <dt>{t("Noisy", { _context, _comment })}</dt>
-  <dd>{(pry.noise ?? 0) > 0 ? t("Yes") : t("No")}</dd>
   <dt>{t("Alarm", { _context, _comment })}</dt>
   <dd>{pry.alarm ? t("Yes") : t("No")}</dd>
+  <dt>{t("Noise", { _context, _comment })}</dt>
+  <dd>{pry.noise ?? 0}</dd>
   <dt>{t("Breakable", { _context, _comment })}</dt>
   <dd>{pry.breakable ? t("Yes") : t("No")}</dd>
-  {#if result}
-    <dt>{t("Becomes", { _context, _comment })}</dt>
-    <dd>
-      <ThingLink id={result} type={resultType} showIcon={false} />
-    </dd>
+  {#if pry.breakable}
+    <dt>{t("Break noise", { _context, _comment })}</dt>
+    <dd>{pry.break_noise ?? 0}</dd>
+    {#if breakItems.length}
+      <dt>{t("Break Items", { _context, _comment })}</dt>
+      <dd>
+        <ul class="comma-separated">
+          {#each breakItems as entry}
+            <li>
+              <ThingLink
+                id={entry.id}
+                type="item"
+                showIcon={false}
+                count={entry.count} />
+            </li>
+          {/each}
+        </ul>
+      </dd>
+    {/if}
   {/if}
   {#if pryItems.length}
     <dt>{t("Pry Items", { _context, _comment })}</dt>
@@ -86,20 +100,10 @@ const _comment = "prying";
       </ul>
     </dd>
   {/if}
-  {#if breakItems.length}
-    <dt>{t("Break Items", { _context, _comment })}</dt>
+  {#if result}
+    <dt>{t("Becomes", { _context, _comment })}</dt>
     <dd>
-      <ul class="comma-separated">
-        {#each breakItems as entry}
-          <li>
-            <ThingLink
-              id={entry.id}
-              type="item"
-              showIcon={false}
-              count={entry.count} />
-          </li>
-        {/each}
-      </ul>
+      <ThingLink id={result} type={resultType} showIcon={false} />
     </dd>
   {/if}
 </dl>
